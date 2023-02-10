07fdc76bcbefae48eb42ed9eab7a12d08a13ef26
Dip Ghosh <dip@storat.com>
2023-02-11 02:24:36 +0600

    versioning added

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/search', [OrderController::class , 'search']);
+Route::get('v1/search', [OrderController::class , 'search']);
